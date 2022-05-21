74ce77d6b87887c6af3bb99dbc117af0252733ba
Carlos Catly <ccatly@gmail.com>
2022-05-21 13:42:46 -0700

    repo made

---
+++ app/src/main/java/com/adidev/breakingbad/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.adidev.breakingbad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adidev.breakingbad.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
